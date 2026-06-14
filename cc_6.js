67b66d05cfcf07e4ce640d1b5b86eaa63002ac10
Krystenae Dennis <krystenaedennis@gmail.com>
2026-06-14 14:56:22 -0400

    add manager class

--- cc_6.js
+++ cc_6.js
@@ -11,3 +11,40 @@ class Employee {
         return `${this.name} works in ${this.department}.`;
     }
 }
+// Manager Class
+
+class Manager extends Employee {
+
+    constructor(name, department, teamSize) {
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+
+    describe() {
+        return `${this.name} manages the ${this.department} department and leads a team of ${this.teamSize} employees.`;
+    }
+}
+
+// Sample Employees and Managers
+
+let employee1 = new Employee(
+    "John Smith",
+    "Sales"
+);
+
+let employee2 = new Employee(
+    "Sarah Johnson",
+    "Marketing"
+);
+
+let manager1 = new Manager(
+    "Michael Brown",
+    "IT",
+    8
+);
+
+let manager2 = new Manager(
+    "Emily Davis",
+    "Finance",
+    5
+);
