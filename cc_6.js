4b83b4a9921c925e6fd3fc814838ff78999ba3d9
Krystenae Dennis <krystenaedennis@gmail.com>
2026-06-14 14:55:26 -0400

    employee class created

---
+++ cc_6.js
@@ -0,0 +1,13 @@
+// Employee Class
+
+class Employee {
+
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    describe() {
+        return `${this.name} works in ${this.department}.`;
+    }
+}
